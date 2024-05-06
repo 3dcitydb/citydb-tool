9762d81b9c97dce1e6d43d930b2af9670b77a13c
Zhihang Yao <zyao@virtualcitysystems.de>
2024-05-06 10:06:15 +0200

    backup

---
+++ citydb-web/src/main/java/module-info.java
@@ -0,0 +1,28 @@
+module org.citydb.web {
+    requires org.citydb.config;
+    requires org.citygml4j.core;
+    requires org.citydb.io;
+    requires org.citydb.io.citygml;
+    requires org.citydb.logging;
+    requires org.citydb.database;
+    requires org.citydb.operation;
+    requires io.swagger.v3.oas.models;
+    requires spring.context;
+    requires spring.beans;
+    requires spring.web;
+    requires spring.boot.autoconfigure;
+    requires spring.boot;
+    requires spring.core;
+
+    exports org.citydb.web;
+    exports org.citydb.web.config;
+    exports org.citydb.web.controller;
+    exports org.citydb.web.model;
+    exports org.citydb.web.service;
+
+    opens org.citydb.web to spring.core;
+    opens org.citydb.web.config to spring.core;
+    opens org.citydb.web.controller to spring.core;
+    opens org.citydb.web.model to spring.core;
+    opens org.citydb.web.service to spring.core;
+}
---
+++ citydb-web/src/main/java/org/citydb/web/SprintDemoApplication.java
@@ -0,0 +1,12 @@
+package org.citydb.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintDemoApplication.class, args);
+    }
+}
