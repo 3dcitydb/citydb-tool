20004c0d6a5707813134c210613fd1e23e424209
Zhihang Yao <zyao@virtualcitysystems.de>
2024-05-24 11:08:57 +0200

    backup

--- citydb-web/src/main/java/org/citydb/web/swagger/SwaggerConfig.java
+++ citydb-web/src/main/java/org/citydb/web/swagger/SwaggerConfig.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
