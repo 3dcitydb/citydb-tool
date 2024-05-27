2934c51a35ec3d68f5c2c47efefc138a79dcd4ab
Zhihang Yao <zyao@virtualcitysystems.de>
2024-05-27 17:39:45 +0200

    backup

--- citydb-web/src/main/java/org/citydb/web/config/Constants.java
+++ citydb-web/src/main/java/org/citydb/web/config/Constants.java
@@ -4,6 +4,8 @@ public class Constants {
 	public static final String SERVICE_CONTEXT_PATH = "/ogcapi";
 	public static final String CONFIG_FILE;
 
+	public static final String GEOJSON_MEDIA_TYPE = "application/json";
+	public static final String CITYGML_MEDIA_TYPE = "application/gml+xml";
 	static {
 		if (System.getenv("WEB_CONFIG_FILE") != null) {
 			CONFIG_FILE = System.getenv("WEB_CONFIG_FILE");
--- citydb-web/src/main/java/org/citydb/web/controller/RequestController.java
+++ citydb-web/src/main/java/org/citydb/web/controller/RequestController.java
@@ -6,15 +6,24 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import org.citydb.web.config.Constants;
 import org.citydb.web.exception.ServiceException;
+import org.citydb.web.schema.Collection;
+import org.citydb.web.schema.Collections;
 import org.citydb.web.schema.Exception;
 import org.citydb.web.schema.LandingPage;
+import org.citydb.web.schema.geojson.FeatureGeoJSON;
 import org.citydb.web.service.CollectionService;
 import org.citydb.web.service.FeatureService;
 import org.citydb.web.service.PageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Objects;
 
 @RestController
 @RequestMapping(value = Constants.SERVICE_CONTEXT_PATH)
@@ -52,7 +61,7 @@ public class RequestController {
     }
 
     @ApiResponse(responseCode = "200", description = "Get the feature collections shared by this API.",
-            content = @Content(mediaType = "application/json", schema = @Schema(implementation = LandingPage.class)))
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Collections.class)))
     @ApiResponse(responseCode = "500", description = "Internal Server Error",
             content = @Content(mediaType = "application/json", schema = @Schema(implementation = Exception.class)))
     @GetMapping("/collections")
@@ -65,11 +74,11 @@ public class RequestController {
     }
 
     @ApiResponse(responseCode = "200", description = "Get the information about the feature collection with id collectionId.",
-            content = @Content(mediaType = "application/json", schema = @Schema(implementation = LandingPage.class)))
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Collection.class)))
     @ApiResponse(responseCode = "500", description = "Internal Server Error",
             content = @Content(mediaType = "application/json", schema = @Schema(implementation = Exception.class)))
     @GetMapping("/collections/{collectionId}")
-    public ResponseEntity<Object> getCollection(@PathVariable("collectionId") String collectionId, HttpServletRequest request) {
+    public ResponseEntity<Object> getCollection(HttpServletRequest request, @PathVariable("collectionId") String collectionId) {
         try {
             return new ResponseEntity<>(collectionService.getCollection(collectionId, request), HttpStatus.OK);
         } catch (ServiceException e) {
@@ -78,18 +87,42 @@ public class RequestController {
     }
 
     @ApiResponse(responseCode = "200", description = "Fetch features of the feature collection with id collectionId.",
-            content = @Content(mediaType = "application/json", schema = @Schema(implementation = LandingPage.class)))
+            content = {
+                    @Content(mediaType = Constants.GEOJSON_MEDIA_TYPE, schema = @Schema(implementation = FeatureGeoJSON.class)),
+                    @Content(mediaType = Constants.CITYGML_MEDIA_TYPE)
+            }
+    )
     @ApiResponse(responseCode = "500", description = "Internal Server Error",
             content = @Content(mediaType = "application/json", schema = @Schema(implementation = Exception.class)))
     @GetMapping("/collections/{collectionId}/items")
     public ResponseEntity<Object> getCollectionFeatures(
+            HttpServletRequest request,
             @PathVariable("collectionId") String collectionId,
             @RequestParam(value = "srid", required = false) Integer srid
     ) {
-        try {
-            return new ResponseEntity<>(featureService.getFeatureCollectionGeoJSON(collectionId, srid), HttpStatus.OK);
-        } catch (ServiceException e) {
-            return new ResponseEntity<>(Exception.of(e), e.getHttpStatus());
+        String contentType = request.getHeader("accept");
+        if (Objects.equals(contentType, Constants.GEOJSON_MEDIA_TYPE)) {
+            try {
+                return new ResponseEntity<>(featureService.getFeatureCollectionGeoJSON(collectionId, srid), HttpStatus.OK);
+            } catch (ServiceException e) {
+                return new ResponseEntity<>(Exception.of(e), e.getHttpStatus());
+            }
+        } else {
+            try {
+                String filePath = "D:\\Daten\\Berlin\\all_wo_app\\data.gml";
+                File file = new File(filePath);
+                long fileSizeInBytes = file.length();
+                FileInputStream fileInputStream = new FileInputStream(file);
+                InputStreamResource inputStreamResource = new InputStreamResource(fileInputStream);
+                return ResponseEntity.ok()
+                        .contentLength(fileSizeInBytes)
+                        .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                        .header("Content-Disposition", "attachment; filename=\"" + filePath + "\"")
+                        .body(inputStreamResource);
+            } catch (java.lang.Exception e) {
+                ServiceException serviceException = new ServiceException(e);
+                return new ResponseEntity<>(Exception.of(serviceException), serviceException.getHttpStatus());
+            }
         }
     }
 }
