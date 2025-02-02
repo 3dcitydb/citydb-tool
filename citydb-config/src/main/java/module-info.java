9a5f44aff6db73b20c91a376b0eb77c636008332
Claus Nagel <cnagel@virtualcitysystems.de>
2025-02-02 10:44:02 +0100

    minor change

--- citydb-config/src/main/java/module-info.java
+++ citydb-config/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module org.citydb.config {
+    requires transitive org.citydb.core;
     requires transitive com.alibaba.fastjson2;
 
     exports org.citydb.config;
     exports org.citydb.config.common;
+    exports org.citydb.config.encoding;
 }
