b8c223131d0560672cad0cd73a3ddc908bc5c166
Claus Nagel <cnagel@virtualcitysystems.de>
2025-02-22 13:34:43 +0100

    updated 3dcitydb schema

--- resources/3dcitydb/postgresql/sql-scripts/citydb-pkg/util.sql
+++ resources/3dcitydb/postgresql/sql-scripts/citydb-pkg/util.sql
@@ -38,10 +38,10 @@ CREATE OR REPLACE FUNCTION citydb_pkg.citydb_version(
   ) RETURNS RECORD AS 
 $$
 SELECT 
-  '5.0.0'::text AS version,
-  5 AS major_version,
-  0 AS minor_version,
-  0 AS minor_revision;
+  '5.0.0-beta.3'::text AS version,
+  5::int AS major_version,
+  0::int AS minor_version,
+  0::int AS minor_revision;
 $$
 LANGUAGE sql IMMUTABLE;
 
