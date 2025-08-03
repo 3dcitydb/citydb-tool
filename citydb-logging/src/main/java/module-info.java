84c79550222cbc983a1943c55ce2c7021820a9da
Claus Nagel <cnagel@virtualcitysystems.de>
2025-08-03 11:44:19 +0200

    updated dependencies

--- citydb-logging/src/main/java/module-info.java
+++ citydb-logging/src/main/java/module-info.java
@@ -1,6 +1,5 @@
 module org.citydb.logging {
     requires org.apache.logging.log4j.core;
-    requires org.slf4j;
     requires transitive org.apache.logging.log4j;
 
     exports org.citydb.logging;
