8cba49f06615b086e5c66879b56be6f883195adb
Claus Nagel <cnagel@virtualcitysystems.de>
2026-01-31 14:16:59 +0100

    use Oracle JARs for compilation

--- citydb-database-oracle/src/main/java/module-info.java
+++ citydb-database-oracle/src/main/java/module-info.java
@@ -1,9 +1,8 @@
 module org.citydb.database.oracle {
-    requires com.oracle.database.jdbc;
-    requires com.oracle.spatial.geometry;
-    requires com.oracle.spatial.util;
     requires org.citydb.database;
-    requires org.locationtech.jts;
+    requires com.oracle.database.jdbc;
+    requires sdoapi;
+    requires sdoutl;
 
     exports org.citydb.database.oracle;
 
