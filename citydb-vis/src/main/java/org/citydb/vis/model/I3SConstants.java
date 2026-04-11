2eaef8ae3b5e4d896ed939a7fc2080da8db3d72a
Zhihang Yao <zhihang.yao@hft-stuttgart.de>
2026-04-11 19:49:22 +0200

    add serialization POJOs for I3S export

---
+++ citydb-vis/src/main/java/org/citydb/vis/model/I3SConstants.java
@@ -0,0 +1,13 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright virtualcitysystems GmbH <https://vc.systems>
+ */
+
+package org.citydb.vis.model;
+
+public final class I3SConstants {
+    public static final int NODES_PER_PAGE = 64;
+
+    private I3SConstants() {
+    }
+}
