aa8da98087a2c749130cd6f1b63947d96e866900
Zhihang Yao <zhihang.yao@hft-stuttgart.de>
2026-04-13 11:25:41 +0200

    refactor: consolidate VIS export commands to visExporter package and rename classes for improved modularity and clarity

---
+++ citydb-cli/src/main/java/org/citydb/cli/visExporter/options/QueryOptions.java
@@ -0,0 +1,85 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright virtualcitysystems GmbH <https://vc.systems>
+ */
+
+package org.citydb.cli.visExporter.options;
+
+import org.citydb.cli.common.AppearanceOptions;
+import org.citydb.cli.common.CountLimitOptions;
+import org.citydb.cli.common.Option;
+import org.citydb.cli.common.TypeNameOptions;
+import org.citydb.cli.common.FilterOptions;
+import org.citydb.cli.exporter.options.LodOptions;
+import org.citydb.query.Query;
+import org.citydb.query.filter.encoding.FilterParseException;
+import picocli.CommandLine;
+
+public class QueryOptions implements Option {
+    @CommandLine.ArgGroup(exclusive = false)
+    private TypeNameOptions typeNameOptions;
+
+    @CommandLine.ArgGroup(exclusive = false)
+    private FilterOptions filterOptions;
+
+    @CommandLine.ArgGroup(exclusive = false)
+    private CountLimitOptions countLimitOptions;
+
+    @CommandLine.ArgGroup(exclusive = false)
+    private LodOptions lodOptions;
+
+    @CommandLine.ArgGroup(exclusive = false)
+    private AppearanceOptions appearanceOptions;
+
+    public Query getQuery() throws FilterParseException {
+        if (typeNameOptions == null
+                && filterOptions == null
+                && countLimitOptions == null
+                && lodOptions == null) {
+            return null;
+        }
+
+        Query query = new Query();
+        if (typeNameOptions != null) {
+            query.setFeatureTypes(typeNameOptions.getTypeNames());
+        }
+
+        if (filterOptions != null) {
+            query.setFilter(filterOptions.getFilter());
+            query.setFilterSrs(filterOptions.getFilterCrs());
+        }
+
+        if (countLimitOptions != null) {
+            query.setCountLimit(countLimitOptions.getCountLimit());
+        }
+
+        if (lodOptions != null) {
+            query.setLodFilter(lodOptions.getLodFilter());
+        }
+
+        return query;
+    }
+
+    public LodOptions getLodOptions() {
+        return lodOptions;
+    }
+
+    public AppearanceOptions getAppearanceOptions() {
+        return appearanceOptions;
+    }
+
+    @Override
+    public void preprocess(CommandLine commandLine) throws Exception {
+        if (countLimitOptions != null) {
+            countLimitOptions.preprocess(commandLine);
+        }
+
+        if (lodOptions != null) {
+            lodOptions.preprocess(commandLine);
+        }
+
+        if (appearanceOptions != null) {
+            appearanceOptions.preprocess(commandLine);
+        }
+    }
+}
