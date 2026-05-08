2f4e5e4ef38a835ea7eaa044518a9c7480a6cb2d
Zhihang Yao <zhihang.yao@hft-stuttgart.de>
2026-05-08 10:51:18 +0200

    refactor(vis): drop Drako Draco compression dependency from I3S writer

--- citydb-vis/src/main/java/module-info.java
+++ citydb-vis/src/main/java/module-info.java
@@ -3,7 +3,6 @@ module org.citydb.vis {
     requires org.citydb.database;
     requires com.alibaba.fastjson2;
     requires org.slf4j;
-    requires drako;
     requires texture.atlas.creator;
     requires java.desktop;
 
