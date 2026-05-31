e5ac4257f667090792e99b22604199a1fbb378f5
Claus Nagel <cnagel@virtualcitysystems.de>
2026-05-31 15:31:01 +0200

    updated dependencies

--- citydb-config/src/main/java/module-info.java
+++ citydb-config/src/main/java/module-info.java
@@ -1,6 +1,5 @@
 module org.citydb.config {
     requires transitive org.citydb.core;
-    requires transitive com.alibaba.fastjson2;
 
     exports org.citydb.config;
     exports org.citydb.config.common;
