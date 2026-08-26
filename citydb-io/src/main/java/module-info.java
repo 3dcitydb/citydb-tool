aadb2d1d9cc4a75db11a23af19c6d807985b1808
Claus Nagel <cnagel@virtualcitysystems.de>
2026-08-26 10:35:47 +0200

    added missing module dependency for Tika 4

--- citydb-io/src/main/java/module-info.java
+++ citydb-io/src/main/java/module-info.java
@@ -1,5 +1,6 @@
 module org.citydb.io {
     requires java.sql;
+    requires com.fasterxml.jackson.databind;
     requires org.apache.tika.serialization;
     requires transitive org.citydb.config;
     requires transitive org.citydb.core;
